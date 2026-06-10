90d4f4ce305a42d04500cb48565a735ec6a39b05
Marcel Veselka <marcel-veselka@users.noreply.github.com>
2026-06-10 12:27:11 +0200

    fix: Simplify page.goto usage in Home Page test
    Removed unnecessary parameters from page.goto call.

--- tests/simple.spec.ts
+++ tests/simple.spec.ts
@@ -7,10 +7,7 @@ test("Home Page", async ({ page }, testInfo) => {
   await wopee.startSuite(process.env.WOPEE_SUITE_NAME || `Simple Example ${new Date().toLocaleString()}`);
   await wopee.startScenario(test.info().title, testInfo);
 
-  await page.goto(process.env.WOPEE_PROJECT_URL || "https://dronjo.wopee.io", {
-    waitUntil: "domcontentloaded",
-    timeout: 30_000,
-  });
+  await page.goto(process.env.WOPEE_PROJECT_URL || "https://dronjo.wopee.io");
   await wopee.trackFullPage({ stepName: "Check Home Page", page });
 
   await wopee.stopScenario();
